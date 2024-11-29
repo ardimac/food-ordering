e74819f871d6731fd41803cb1ee16249e0c213d4
ardimac <ardi.tri.yunansyah@gmail.com>
2024-11-29 14:50:16 +0700

    add bastion,postgres,jenkins tf

--- infrastructure/instance.tf
+++ infrastructure/instance.tf
@@ -16,9 +16,48 @@ resource "aws_autoscaling_group" "main" {
 # Create a Launch Template
 resource "aws_launch_template" "main" {
   name            = "app-template"
-  image_id        = "ami-00c0b98389042f9d5" # Replace with your desired AMI
+  image_id        = "ami-0b037f98da520a29a" # Replace with your desired AMI
   instance_type   = "t2.micro"
   key_name        = "testapp"
   vpc_security_group_ids = [aws_security_group.sg-private-application.id]
-  user_data = file("install_nginx.sh")
+  user_data = filebase64("userdata.sh")
 }
+
+# Create emergency bastion
+resource "aws_instance" "bastion" {
+  ami                    = "ami-047126e50991d067b"
+  instance_type          = "t2.micro"
+  key_name               = "testapp"
+  subnet_id              = aws_subnet.public_subnet-a.id
+  vpc_security_group_ids = [aws_security_group.sg-public-traffic.id]
+  tags = {
+    Name = "bastion"
+  }
+  associate_public_ip_address = "true"
+}
+
+# Create jenkins
+resource "aws_instance" "jenkins" {
+  ami                    = "ami-0b25a263e0c4729cb"
+  instance_type          = "t2.medium"
+  key_name               = "testapp"
+  subnet_id              = aws_subnet.public_subnet-b.id
+  vpc_security_group_ids = [aws_security_group.sg-public-traffic.id]
+  tags = {
+    Name = "jenkins"
+  }
+  associate_public_ip_address = "true"
+}
+
+# Create postgres
+resource "aws_instance" "postgres" {
+  ami                    = "ami-059edcde4b66b8e2e"
+  instance_type          = "t2.micro"
+  key_name               = "testapp"
+  subnet_id              = aws_subnet.private_subnet-a.id
+  vpc_security_group_ids = [aws_security_group.sg-private-application.id]
+  tags = {
+    Name = "postgres"
+  }
+}
+
